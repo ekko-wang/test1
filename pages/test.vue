178d873903eba29b116fbb7ce14c284ccc5d26f7
Ekko <15611637909@163.com>
2022-12-31 16:59:58 +0800

    init

---
+++ pages/test.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    test
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  mounted() {
+    this.$nextTick(() => {
+      this.$nuxt.$loading.start()
+
+      setTimeout(() => this.$nuxt.$loading.finish(), 6500)
+    })
+  }
+}
+</script>
